d35bf3d8dfadc1fafe3ee897576f4e9707ea7319
Misael Gomes <misaelweb@hotmail.com>
2020-10-29 11:26:43 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -3,16 +3,7 @@
 
 #versões
 ELASTIC=7.x
-MYSQL=ps80
 PCRE_VERSION=8.44
-
-#instalação 03 - MYSQL
-cd $HOME
-wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
-sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
-sudo percona-release setup $MYSQL
-sudo apt-get install percona-server-server
-apt-get install -y phpmyadmin
 
 #instalação 04 - ELASTICSEARCH
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
