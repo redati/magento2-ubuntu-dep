94fd44d944ab71a2bc4dced03a99178390ae5a1b
Misael Gomes <misaelweb@hotmail.com>
2020-10-29 11:24:00 -0300

    Create 01-dep.sh

---
+++ 01-dep.sh
@@ -0,0 +1,6 @@
+# instalação 01 - dependencias
+sudo apt-get -y update && apt-get -y upgrade
+sudo apt-get install -y autoconf automake libtool zlib1g-dev unzip libssl-dev  libxml2-dev libcurl4-openssl-dev git build-essential flex gcc make pkg-config g++ libfl-dev bison
+sudo apt-get install -y software-properties-common curl openssl ca-certificates postfix gnupg2 default-jdk rabbitmq-server net-tools
+sudo apt-get install -y optipng gifsicle jpegoptim webp libgeoip-dev luajit nginx-common redis-server
+sudo apt-get install -y libvips libvips-dev libatomic-ops-dev libjemalloc-dev libperl-dev libgd-dev libxslt-dev libxml2-dev
