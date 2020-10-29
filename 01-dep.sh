80fe44151b841a0e2410632963ec42243cfd9753
Misael Gomes <misaelweb@hotmail.com>
2020-10-29 11:25:03 -0300

    Update 01-dep.sh

--- 01-dep.sh
+++ 01-dep.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # instalação 01 - dependencias
 sudo apt-get -y update && apt-get -y upgrade
 sudo apt-get install -y autoconf automake libtool zlib1g-dev unzip libssl-dev  libxml2-dev libcurl4-openssl-dev git build-essential flex gcc make pkg-config g++ libfl-dev bison
