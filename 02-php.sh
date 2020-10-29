8aee9fd3d3fa560b2748db6bba5f312f7cd55ef9
Misael Gomes <misaelweb@hotmail.com>
2020-10-29 12:08:43 -0300

    Update 02-php.sh

--- 02-php.sh
+++ 02-php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd &HOME
 
 #versões
 PHP_V=php7.4
@@ -15,3 +16,5 @@ sudo apt-get install -y libvips libvips-dev libatomic-ops-dev libjemalloc-dev li
 sudo apt-get install ${PHP_V}-xmlrpc ${PHP_V}-fpm ${PHP_V}-opcache ${PHP_V}-gd ${PHP_V}-zip ${PHP_V}-curl ${PHP_V}-mbstring ${PHP_V}-redis ${PHP_V}-xml ${PHP_V}-soap ${PHP_V}-dev ${PHP_V}-imap ${PHP_V}-pdo ${PHP_V}-mysql ${PHP_V}-mysqli ${PHP_V}-bcmath ${PHP_V}-intl ${PHP_V}-xsl ${PHP_V}-apcu ${PHP_V}-memcached  ${PHP_V}-imagick
 sudo pecl install lzf
 sudo pecl install vips
+sudo curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
